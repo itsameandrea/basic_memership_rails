9366981c0f7ee164bc7adabb911e1bc6cd57b110
andrearocca <andrea.rocca3@gmail.com>
2016-01-29 14:39:46 +0000

    Added create method to profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,11 +7,31 @@ class ProfilesController < ApplicationController
         
         @profile = @user.build_profile
     end
-    
-    def create
-        
-    
-    
 
+    def create
+       
+       @user = current_user
+       
+       @profile = @user.build_profile(profile_params)
+    
+       if @profile.save
+
+
+          flash[:success] = "Profile updated"
+          redirect_to user_path
+          
+      else
+          flash[:danger] = "Error occurred, profile hasn't been updated"
+          render action: :new
+      end
+      
+      
+    end
+    
+    
+    private
+        def profile_params
+            params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+        end 
 
 end
