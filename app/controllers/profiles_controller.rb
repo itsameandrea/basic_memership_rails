6beb6d6216320dc536b6965de3736dac77ed54c8
andrearocca <andrea.rocca3@gmail.com>
2016-01-29 14:02:23 +0000

    Added current user settings to profile view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,17 @@
 class ProfilesController < ApplicationController
     
+    def new
+        # form where a user can fill out his own profile.
+    
+        @user = current_user
+        
+        @profile = @user.build_profile
+    end
+    
+    def create
+        
+    
+    
 
 
 end
