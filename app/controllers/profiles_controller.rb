8c74de639730302e187272709fe0cf142ffca1ae
andrearocca <andrea.rocca3@gmail.com>
2016-01-30 08:40:41 +0000

    Secured user profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,61 +1,47 @@
 class ProfilesController < ApplicationController
-    
-    def new
-        # form where a user can fill out his own profile.
-    
-        @user = current_user
-        
-        @profile = Profile.new
+  before_action :authenticate_user!
+  before_action :only_current_user
+  
+  def new
+    # form where a user can fill out their own profile.
+    @user = User.find( params[:user_id] )
+    @profile = Profile.new
+  end
+  
+  def create 
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      render action: :new
     end
-
-    def create
-       
-       @user = current_user
-       
-       @profile = @user.build_profile(profile_params)
-    
-       if @profile.save
-
-          flash[:success] = "Profile updated"
-          redirect_to user_path(current_user)
-          
-      else
-          flash[:danger] = "Error occurred, profile hasn't been updated"
-          render action: :new
-      end
-      
-      
+  end
+  
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+  end
+  
+  def update
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      render action: :edit
     end
-    
-    def edit
-       
-       @user = current_user
-       
-       @profile = @user.profile
-        
+  end
+  
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
     end
-    
-    def update
-        
-        @user = current_user
-        
-        @profile = @user.profile
-        
-        if @profile.update_attributes(profile_params)
-            
-          flash[:success] = "Profile updated"
-          redirect_to user_path(current_user)
-          
-        else
-          flash[:danger] = "Error occurred, profile hasn't been updated"
-          render action: :edit
-        end
+  
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
     end
-    
-    
-    private
-        def profile_params
-            params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
-        end 
-
 end
